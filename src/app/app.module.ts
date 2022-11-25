8943b30644d577912ee175e8e50c154e04e10b13
Christian Saavedra <christian.saavedra@correounivalle.edu.co>
2022-11-25 17:14:17 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorModule } from './Contador/contador.module';
+import { HeroeModule } from './Heroes/Heroe.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContadorModule,
+    HeroeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
